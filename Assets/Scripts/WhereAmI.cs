e0520ac1fcd783ddc70bd045e2e188f198c89913
Victor Arnaud <victorhad@gmail.com>
2023-01-15 16:55:15 -0300

    Ajuste no layout dos dispositivos

--- Assets/Scripts/WhereAmI.cs
+++ Assets/Scripts/WhereAmI.cs
@@ -9,6 +9,8 @@ public class WhereAmI: MonoBehaviour {
     public int shopSceneIndex;
     public bool isStageScene;
     [SerializeField] private GameObject UIManager, GameManager;
+    private float aspect = 1.75f;
+    private float orthographicSize = 5;
 
     public static WhereAmI instance;
 
@@ -28,10 +30,20 @@ public class WhereAmI: MonoBehaviour {
         startSceneIndex = SceneUtility.GetBuildIndexByScenePath("Scenes/Start Scene");
         menuStageSceneIndex = SceneUtility.GetBuildIndexByScenePath("Scenes/Stages Menu");
         shopSceneIndex = SceneUtility.GetBuildIndexByScenePath("Scenes/Shop");
+
         if (sceneIndex != startSceneIndex && sceneIndex != menuStageSceneIndex && sceneIndex != shopSceneIndex) {
             isStageScene = true;
             Instantiate(UIManager);
             Instantiate(GameManager);
+            // Ajuste de resolução do game para todos os dispositivos
+            Camera.main.projectionMatrix = Matrix4x4.Ortho(
+                -orthographicSize * aspect,
+                orthographicSize * aspect,
+                -orthographicSize,
+                orthographicSize,
+                Camera.main.nearClipPlane,
+                Camera.main.farClipPlane
+            );
         } else {
             isStageScene = false;
         }
